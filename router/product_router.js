ab6af252bec37d492e6c777003957ef62fb8e0bc
pranavimalempati <118198427+pranavimalempati@users.noreply.github.com>
2022-12-07 11:58:29 +0530

    Update product_router.js

--- router/product_router.js
+++ router/product_router.js
@@ -3,7 +3,7 @@ const product = require('../controller/products_controller')
 
 const router =require('express').Router()
 router.post('/create',product.add)
-router.get('/get/:id',product.getAll)
+router.get('/get',product.getAll)
 router.delete('/delete/:id',product.remove)
 router.post('/find',product.findmany)
 
